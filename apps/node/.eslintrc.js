845e16815cbafdc4c4bb2d28543a82eefe13861d
bontaramsonta <18cs113@gmail.com>
2022-07-30 12:16:51 +0530

    node: filter from different types of chats

--- apps/node/.eslintrc.js
+++ apps/node/.eslintrc.js
@@ -1,15 +1,18 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "standard"
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: ['standard'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'space-before-function-paren': {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
     }
+  }
 }
--- apps/node/src/main.js
+++ apps/node/src/main.js
@@ -4,10 +4,11 @@ let YOUTUBE_CHAT = []
 
 let DB_CHAT = []
 
-const _insertChat = () => {
+const _insertChat = (type) => {
   const index = YOUTUBE_CHAT.length
   const chat = {
     chat: `Hello${index + 1}`,
+    type,
   }
   YOUTUBE_CHAT.push(chat)
 }
@@ -19,10 +20,15 @@ const getChatCount = () => {
   return YOUTUBE_CHAT.length
 }
 
-let pageIndex = 0
-// let changePage = false
-let db_count = DB_CHAT.length
+const getDbChatSlice = (pageIndex) => {
+  return DB_CHAT.slice(pageIndex * M, (pageIndex + 1) * M)
+}
+
+let _status = 'liveStarting'
 let _run_count = 0
+let db_count = DB_CHAT.length
+let changePage = false
+let pageIndex = 0
 const _l = (...args) => {
   console.log(`[${_run_count}]`, ...args)
 }
@@ -30,40 +36,69 @@ function run() {
   _run_count++
   _l('------')
   const yt_count = getChatCount()
+  const page_limit = M * (pageIndex + 1)
   _l(db_count, yt_count)
-  if (db_count < yt_count) {
+  if (_status == 'live' && yt_count > db_count) {
     _l('[get chats]')
-    const page_limit = M * (pageIndex + 1)
-    const upper = Math.min(yt_count, page_limit)
-    _l('[upper]', upper)
-    let chats = getChats(pageIndex)
-    _l('[chats]', pageIndex, chats)
-    const lower = db_count % M
-    let slicedchats = chats.slice(lower, upper)
-    _l('[delta]', slicedchats.length)
-    _l('[slicedchats]', lower, upper, slicedchats)
-    db_count += slicedchats.length
-    DB_CHAT.push(...slicedchats)
-    // _l('[chats]', chats)
-    if (yt_count >= page_limit) {
-      _l('[next page]', ++pageIndex)
+    if (changePage) {
+      _l('[next page fetched]')
+      changePage = false
     }
+    const chats = getChats(pageIndex)
+    const dbchats = getDbChatSlice(pageIndex)
+    const textchats = chats.filter((chat) => chat.type == 'text')
+    const actual_yt_count = textchats.length
+    const actual_db_count = dbchats.length
+    _l('[text chats]', actual_yt_count, textchats)
+    if (actual_yt_count > actual_db_count) {
+      const slicedtextchats = textchats.slice(actual_db_count % M)
+      _l('[sliced text chats]', actual_db_count % M, slicedtextchats)
+      DB_CHAT.push(...slicedtextchats)
+    }
+    if (yt_count >= page_limit) {
+      _l('[ready to change page]')
+      ++pageIndex // inc pageIndex
+      changePage = true
+    }
+    db_count = yt_count
+    _l('[pI]', changePage, pageIndex)
+    _l('[db]', db_count, DB_CHAT)
+    _l('[yt]', yt_count, YOUTUBE_CHAT)
+  } else if (_status != 'live') {
+    _l('[not live]', _status)
+  } else {
+    _l('[nothing to do]')
   }
-  _l('[pI]', pageIndex)
-  _l('[db]', DB_CHAT.length)
-  _l('[yt]', YOUTUBE_CHAT.length)
   _l('------\n')
 }
 
 //events timeline
-_insertChat()
-_insertChat()
-run()
-_insertChat()
-_insertChat()
-_insertChat()
-run()
-run()
+run() // 1 --> not live
+_status = 'live'
+_insertChat('text')
+_insertChat('delete')
+run() // 2 --> live with mixed chats
+_insertChat('text')
+_insertChat('text')
+_insertChat('supchat')
+run() // 3 --> overflow page
+_insertChat('banned')
+_insertChat('sticker')
+run() // 4  --> no text chats
+run() // 5  --> no chats
+_insertChat('text')
+_insertChat('text')
+_insertChat('supchat')
+run() // 6  --> next page logic
+_insertChat('banned')
+_insertChat('sticker')
+run() // 7
+_insertChat('text')
+_insertChat('ended')
+run() // 8
+run() // 9
+_status = 'complete'
+run() // 10
 
 console.log('[final yt state]', YOUTUBE_CHAT)
 console.log('[final db state]', DB_CHAT)
