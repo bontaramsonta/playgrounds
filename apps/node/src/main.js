1bb0d722cc25d2b43f146eb211a489f65c749c5b
bontaramsonta <18cs113@gmail.com>
2022-08-09 15:11:21 +0530

    node: mongoscript: fix urls for baaz

--- apps/node/src/main.js
+++ apps/node/src/main.js
@@ -1,144 +1,68 @@
-const PAGE_SIZE = 8
+import 'dotenv/config'
+import { MongoClient } from 'mongodb'
 
-let YOUTUBE_CHAT = []
+const client = new MongoClient(process.env.MONGO)
+// const local = new MongoClient('mongodb://localhost:27017')
 
-let DB_CHAT = []
-
-const _insertChat = (type) => {
-  const index = YOUTUBE_CHAT.length
-  const chat = {
-    id: Math.random().toString(36).substring(2, 15),
-    chat: `Hello${index + 1}`,
-    type,
-  }
-  YOUTUBE_CHAT.push(chat)
-}
-
-const getChats = (pageIndex) => {
-  return {
-    totalResultsPerPage: PAGE_SIZE,
-    totalResults: YOUTUBE_CHAT.length,
-    chats: YOUTUBE_CHAT.slice(
-      pageIndex * PAGE_SIZE,
-      (pageIndex + 1) * PAGE_SIZE
-    ),
-  }
-}
-
-const getDbChatSlice = (pageIndex) => {
-  return DB_CHAT.slice(pageIndex * PAGE_SIZE, (pageIndex + 1) * PAGE_SIZE).map(
-    (i) => i.id
-  )
-}
-
-let _run_count = 0
-let db_count = 0
-let changePage = false
-let pageIndex = 0
-const _l = (...args) => {
-  console.log(`[${_run_count}]`, ...args)
-}
-function run() {
-  _run_count++
-  _l('------')
-  if (changePage) {
-    // include next page token in chat input
-    _l('fetch new chats', pageIndex)
-    changePage = false
-  }
-  const LiveChatresponse = getChats(pageIndex)
-  const yt_count = LiveChatresponse.chats.length + pageIndex * PAGE_SIZE
-  _l(
-    'un-filtered chats',
-    LiveChatresponse.chats.map((c) => `${c.type}:${c.chat}`)
-  )
-  let chats = LiveChatresponse.chats
-    .filter((i) => i.type == 'text')
-    .map((i) => ({
-      chatId: i.id,
-      broadcastId: 'example-broadcast-id',
-      message: i.chat,
-      source: '1',
-      username: '2',
-      timestamp: new Date(),
-    }))
-  _l(
-    'filtered',
-    chats.map((c) => c.message)
-  )
-  const seperator = db_count
-  const new_chats = chats.slice(seperator)
-  _l({ new_chats, l: new_chats.length, seperator })
-  if (new_chats.length > 0) {
-    // convert to id array
-    const new_ids = new_chats.map((c) => c.message)
-    DB_CHAT.push(...new_ids)
-    // increment db counter
-    db_count += new_ids.length
-    // send new chats to users
-  } else {
-    _l('===> no new chats')
-  }
-  const page_limit = (pageIndex + 1) * PAGE_SIZE
-  _l(db_count, yt_count, page_limit)
-  if (yt_count >= page_limit) {
-    changePage = true
-    pageIndex++
-    // reset count
-    db_count = 0
-    _l('change page next request', pageIndex)
-  }
-  // save all saves to db
-  _l('db_count', db_count)
-  _l('changePage', changePage)
-  _l('pageIndex', pageIndex)
-  // _l('nextChatToken', 'example-next-chat-token')
-
-  _l('------\n')
-}
-
-//events timeline
-run() // 1 --> not live
-_insertChat('text')
-_insertChat('delete')
-run() // 2 --> live with mixed chats
-_insertChat('text')
-_insertChat('text')
-_insertChat('supchat')
-run() // 3 --> overflow page
-_insertChat('banned')
-_insertChat('sticker')
-run() // 4  --> no text chats
-run() // 5  --> no chats
-_insertChat('text')
-_insertChat('text')
-_insertChat('supchat')
-run() // 6  --> next page logic
-_insertChat('banned')
-_insertChat('sticker')
-run() // 7
-_insertChat('text')
-_insertChat('text')
-_insertChat('supchat')
-run() // 3 --> overflow page
-_insertChat('banned')
-_insertChat('sticker')
-run() // 4  --> no text chats
-run() // 5  --> no chats
-_insertChat('text')
-_insertChat('text')
-_insertChat('supchat')
-run() // 6  --> next page logic
-_insertChat('banned')
-_insertChat('sticker')
-run() // 7
-_insertChat('text')
-_insertChat('ended')
-run() // 8
-run() // 10
-
-console.log(
-  '[final yt state]',
-  YOUTUBE_CHAT.map((i) => `${i.type}:${i.chat}`)
-)
-console.log('[final db state]', DB_CHAT)
+client.once('connectionReady', async () => {
+  console.log('[connected]')
+  let users = client.db('atii').collection('users')
+  let userdetails = client.db('atii').collection('userdetails')
+  const userdocs = await users.aggregate(
+    [
+      {
+        $match: {
+          username: { $exists: true }
+        }
+      },
+      // { $limit: 2 },
+      {
+        $lookup: {
+          from: 'userdetails',
+          localField: 'userDetails',
+          foreignField: '_id',
+          as: 'details',
+        }
+      },
+      {
+        $project: {
+          details: 1,
+          username: 1,
+        }
+      }
+    ]).toArray()
+  console.dir(userdocs, { depth: 10 })
+  await Promise.all(userdocs.map(d => {
+    return new Promise(async (resolve, reject) => {
+      let name = d.username
+      let linkarr = d.details[0].wishlistLink?.split('/shop/')
+      if (linkarr && linkarr.at(linkarr?.length - 1)) {
+        linkarr[linkarr?.length - 1] = name
+        let newurl = encodeURI(`https://shop.baaz.live/shop/${name.trim()}`)
+        console.log(`[${name}]`, newurl)
+        // write to userdetails
+        await userdetails.updateOne({ _id: d.details[0]._id }, {
+          $set: {
+            wishlistLink: newurl
+          }
+        }, { upsert: true })
+        resolve(1)
+      } else {
+        console.log('[x]', d)
+        resolve(0)
+      }
+    })
+  }))
+  setTimeout(() => { client.close() }, 10000) // 10 seconds grace time
+})
+client.once('connectionClosed', () => {
+  console.log('[closed]')
+})
+// local.once('connectionReady', () => {
+//   console.log('[local connected]')
+// })
+client.connect()
+// local.once('error', () => {
+//   console.log('[local err]')
+// })
+// local.connect()
